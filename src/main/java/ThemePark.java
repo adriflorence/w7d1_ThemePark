11c168bb13e882f9011ac6bbb0df4c074a5b457c
Adri <forczekadri@gmail.com>
2018-06-25 21:55:17 +0100

    Dani szeretlek

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -2,11 +2,22 @@ import java.util.ArrayList;
 
 public abstract class ThemePark {
 
+    ArrayList<IReviewed> allPlaces;
+
     public void visit(Visitor visitor, Attraction attraction) {
         if (visitor.getMoney() >= attraction.defaultPrice()) {
             visitor.takeMoney(attraction.defaultPrice());
         }
     }
 
-//    public ArrayList<IReviewed> getAllReviewed();
+    public void addPlaceToThemePark(IReviewed place){
+        allPlaces.add(place);
+    }
+
+    public ArrayList<IReviewed> getAllReviewed(){
+        ArrayList<IReviewed> reviewedPlaces = new ArrayList<IReviewed>();
+
+
+        return reviewedPlaces;
+    }
 }
